9da5f3be848b56b82bb16233cf01f1bc2b3d3a85
appdevds-explorer01 <155011040+appdevds-explorer01@users.noreply.github.com>
2023-12-28 16:49:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ try:
     streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
-streamlit.write('The user entered', fruit_choice)
+# streamlit.write('The user entered', fruit_choice)
 
 # import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
